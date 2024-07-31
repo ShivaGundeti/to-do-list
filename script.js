1ba1753b87e742e079cd2bc7540cd747afb8462c
ShivaGundeti <shivagundeti1403@gmail.com>
2024-07-31 12:06:11 +0530

    added to git

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
-const value = document.getElementById("form-control").value;
 
-console.log(value);
-//nenu ikkada input box nunchi value teestunna
-const task = document.getElementById("button-addon2");
-
-
-var table = document.getElementById("table");
+var arr = [];
+let id = 1;
 function s() {
-    console.log(document.getElementById("form-control").value);
-    // Create an empty <tr> element and add it to the 1st position of the table:
-    var row = table.insertRow(1);
-    row.setAttribute("id","table2")
-    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2)
-    // Add some text to the new cells:
-    cell1.innerHTML = '1';
-    cell2.textContent = `value`;
-    cell3.innerHTML = `<button type="button" class="btn btns btn-edit btn-primary"><i class="ri-edit-line"></i></button> &nbsp <button type="button" class="btn btns btn-bin btn-primary"><i class="ri-delete-bin-line"></i></button>`
-    const btns = document.querySelectorAll(".btns");
-    let count = 1
-    console.log(btns);
-    btns.forEach(function (btn) {
-        btn.addEventListener('click', function (e) {
-            const x = e.currentTarget.classList;
-            console.log(x);
-            if (x.contains("btn-edit")) {
-                document.getElementById("table2").setAttribute('contenteditable', 'true')
-            } 
-            else if(x.contains("btn-bin")) {
-                // console.log(count);
-                document.getElementById("table").deleteRow(count)
-                count++;  
-            }
-        })
-    })
+    const input_value = document.getElementById("input-value").value;
+    arr.push({ id: id++, task: input_value })
+Table()
+
 }
 
-// const node = document.createElement("td");
+function deleteTask(id){
 
-// const textnode = document.createTextNode('hi');
+arr=arr.filter(ele=>ele.id!=id)
+console.log(arr);
+Table()
+}
+function editTask(){
 
-// node.appendChild(textnode);
-
-// document.getElementById("table").appendChild(node);
+}
 
 
+function Table(){
+    const table = document.getElementById("demo-table");
+    let html_data = `<table width="100%"><tr>
+     <th width="20%">#</th>
+            <th width="60%">Task</th>
+            <th width="20%">Action</th>
+</tr>`
+for (let item of arr) {
+html_data += `<tr><td>${item.id}</td><td>
+${
+true?'<input type="text" value='+item.task+'>':item.task
+}
+</td><td>
+<button type="button" class="btn btn-primary ms-3" onclick="editTask(${item.id})"><i class="ri-edit-line"></i></button>  <button type="button" class="btn btn-primary" onclick="deleteTask(${item.id})"><i class="ri-delete-bin-line"></i></button>
+</td></tr>`
+}
+html_data += "</table>"
+table.innerHTML = html_data
+}
 
 
 
